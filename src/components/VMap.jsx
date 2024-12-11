c95910806041d820d14782a9ff6321eb5388fb36
ixio0330 <ixio0330@gmail.com>
2024-12-11 14:12:53 +0900

    💄 매장 위치 지도에 마크 표시

--- src/components/VMap.jsx
+++ src/components/VMap.jsx
@@ -14,7 +14,6 @@ import { useEffect } from 'react'
 
 export default function VMap({ stores }) {
   useEffect(() => {
-    // create Map instance
     const map = new Map({
       controls: defaults({ zoom: true, rotate: false }).extend([]),
       layers: [
@@ -27,39 +26,42 @@ export default function VMap({ stores }) {
       ],
       target: 'vmap',
       view: new View({
-        center: fromLonLat([127.189972804, 37.723058796]),
+        center: fromLonLat([126.92247308499337, 37.52572414089632]),
         zoom: 15,
       }),
     })
 
-    let marker = new Feature({
-      geometry: new Point(fromLonLat([127.189972804, 37.723058796])),
+    const markers = []
+
+    stores?.forEach(({ name, latitude, longitude }) => {
+      let marker = new Feature({
+        geometry: new Point(fromLonLat([longitude, latitude])),
+      })
+
+      let myStyle = new Style({
+        text: new Text({
+          text: name,
+          font: 'bold 14px sans-serif',
+          offsetY: 10,
+        }),
+
+        image: new Icon({
+          anchor: [0.5, 1],
+          src: 'http://map.vworld.kr/images/ol3/marker_blue.png',
+          scale: 1.0,
+        }),
+      })
+
+      marker.setStyle(myStyle)
+      markers.push(marker)
     })
 
-    let myStyle = new Style({
-      text: new Text({
-        text: '마크',
-        font: 'bold 14px sans-serif',
-        offsetY: 10,
-      }),
-
-      image: new Icon({
-        anchor: [0.5, 1],
-        src: 'http://map.vworld.kr/images/ol3/marker_blue.png',
-        scale: 1.0,
-      }),
-    })
-
-    marker.setStyle(myStyle)
-
-    // create a vector layer and add the marker feature to it
     const markerLayer = new Vector({
       source: new VectorSource({
-        features: [marker],
+        features: [...markers],
       }),
     })
 
-    // add myLayer
     map.addLayer(markerLayer)
   })
 
