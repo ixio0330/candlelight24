fdbd07b85dafbec718b4e4e010277bf37c86fd12
ixio0330 <ixio0330@gmail.com>
2024-12-12 17:34:40 +0900

    👔 오늘 날짜 한국 시간대 적용

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,3 +1,6 @@
 export function getToday() {
-  return new Date().toISOString().split('T')[0]
+  const now = new Date()
+  const offset = 9 * 60 * 60 * 1000
+  const kstTime = new Date(now.getTime() + offset)
+  return kstTime.toISOString().split('T')[0]
 }
